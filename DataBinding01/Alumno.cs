7001739198fe5e142eafcdb252ecb1e2bba9f431
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-08-27 16:21:04 -0600

    Agregar archivos de proyecto.

---
+++ DataBinding01/Alumno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding01
+{
+		public class Alumno : INotifyPropertyChanged
+		{
+			
+			public event PropertyChangedEventHandler PropertyChanged;
+
+			string nombre = string.Empty;
+			public string Nombre
+		{
+			get => nombre;
+			set
+			{
+				if (nombre == value)
+					return;
+				nombre = value;
+				OnPropertyChanged(nameof(Nombre));
+				OnPropertyChanged(nameof(MostrarNombre));
+			}
+		}
+			public string MostrarNombre => $"Nombre Ingresado:{Nombre}";
+
+			void OnPropertyChanged(string nombre)
+			{
+			
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+			}
+		}
+	
+}
---
+++ DataBinding01/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DataBinding01;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
